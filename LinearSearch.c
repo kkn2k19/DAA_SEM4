4d7b499387b33ceecd0d30dc1aee5bf7151975de
Karan Kumar Nonia <83773820+kkn2k19@users.noreply.github.com>
2024-03-21 00:05:54 +0530

    Create LinearSearch.c

---
+++ LinearSearch.c
@@ -0,0 +1,46 @@
+//21.03.2024
+
+#include<stdio.h>
+
+int main()
+{
+    int n, i, arr[20], key;
+    printf("Enter size of the Array : ");
+    scanf("%d", &n);
+    printf("Enter the elements of Sorted Array : ");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("Array : ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\nEnter the value to search : ");
+    scanf("%d", &key);
+    int index;
+    index = linearSearch(arr, n, key); 
+    if (index == -1) {
+        printf("Not Found\n");
+    } else {
+        printf("Found at index : %d\n", index);
+    }
+    return 0;
+}
+
+int linearSearch(int arr[], int n, int key) 
+{
+    int i;
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// OUTPUT ---
+// Enter size of the Array : 5
+// Enter the elements of Sorted Array : 1 2 3 4 5 
+// Array : 1 2 3 4 5 
+// Enter the value to search : 3 
+// Found at index : 2
